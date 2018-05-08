cd302b8663f54d9b413c988b5e02bd4af234b1b6
jason <jasonqiao36@gmail.com>
2018-05-08 16:41:06 +0800

    增加 邮件头部信息

--- send_email/simple.py
+++ send_email/simple.py
@@ -1,28 +1,27 @@
 import smtplib
+from email import encoders
+from email.header import Header
 from email.mime.text import MIMEText
+from email.utils import parseaddr, formataddr
 
+
+def _format_addr(s):
+    name, addr = parseaddr(s)
+    return formataddr((Header(name, 'utf8').encode(), addr))
+
+
+from_addr = 'xxx@qq.com'
+passwd = 'xxx'
+to_addrs = ['xxx@gmail.com']
 smtp_server = 'smtp.qq.com'
 
+msg = MIMEText('hello, send by python ...', 'plain', 'utf8')
+msg['From'] = _format_addr(f'Jasonqiao36 <{from_addr}>')
+msg['To'] = _format_addr(f'管理员 <{to_addrs}>')
+msg['Subject'] = Header('来自Jason的问候..', 'utf8').encode()
 
-def send_mail(from_addr, to_addrs, msg, passwd):
-    """
-
-    :param from_addr:发送方
-    :param to_addrs:接受方（list类型）
-    :param msg:正文
-    :param passwd:邮箱密码（授权码）
-    :return:
-    """
-    server = smtplib.SMTP_SSL(smtp_server, 465)
-    server.set_debuglevel(1)
-    server.login(from_addr, passwd)
-    server.sendmail(from_addr, to_addrs, msg.as_string())
-    server.quit()
-
-
-if __name__ == '__main__':
-    from_addr = 'xxx@qq.com'
-    passwd = 'xxx'
-    to_addrs = ['xxx@qq.com']
-    msg = MIMEText('main content ...', 'plain', 'utf8')
-    send_mail(from_addr, to_addrs, msg, passwd)
+server = smtplib.SMTP_SSL(smtp_server, 465)
+server.set_debuglevel(1)
+server.login(from_addr, passwd)
+server.sendmail(from_addr, to_addrs, msg.as_string())
+server.quit()
